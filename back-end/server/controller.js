0b6876af20cccac42a752bc39053f8dcc91797df
Linh Hoang <Linh.Hoang@devmounta.in>
2021-06-18 17:19:07 -0500

    updated after assessment review

--- back-end/server/controller.js
+++ back-end/server/controller.js
@@ -1,21 +1,16 @@
 const pics = require('./pics.json')
-
+const itemsArr = ['nails','hammer','parka'];
 
 module.exports = {
     getCompliment: function (req, res){
         const compliments = ["Gee, you're a smart cookie!",
-        "Cool shirt!",
-        "Your Javascript skills are stellar.",
-];
-
-// choose random compliment
+        "Cool shirt!","Your Javascript skills are stellar.",];
         let randomIndex = Math.floor(Math.random() * compliments.length);
         let randomCompliment = compliments[randomIndex];
-
         res.status(200).send(randomCompliment);
     },
     getFortune: function(req,res){
-        const fortunes = ["The fortune you seek is in another cookie","We don’t know the future, but here’s a cookie", "That wasn’t chicken", "This cookie contains 117 calories", "Don’t eat the paper",];
+        const fortunes = ["The fortune you seek is in another cookie","We don’t know the future, but here’s a cookie", "That wasn’t chicken", "This cookie contains 117 calories", "Don’t eat the paper"];
         let randomInd = Math.floor(Math.random() * fortunes.length);
         let randomFortune = fortunes[randomInd]
         res.status(200).send(randomFortune)
@@ -26,5 +21,19 @@ module.exports = {
     },
     getImg: function(req,res){
         res.status(200).send(pics)
+    },
+    getItem: function(req,res){
+        res.status(200).send(itemsArr)
+    },
+    addItem: function(req, res){
+        const{ newItem } = req.body;
+        itemsArr.push(newItem);
+        res.sendStatus(200);
+    },
+    deleteItem: function(req,res){
+        const{ indexPos } = req.params;
+        const removedItem = itemsArr.splice(+indexPos, 1);
+        res.status(200).send(removedItem)
     }
+
 }
--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const ctrl = require('./controller.js')
+const ctrl = require("./controller.js");
 const app = express();
-const {getCompliment, getFortune, getForm, getImg} = ctrl;
-
+const {getCompliment, getFortune, getForm, getImg, getItem, addItem, deleteItem} = ctrl;
 
 app.use(cors());
 
@@ -12,6 +11,9 @@ app.use(express.json()); // When we want to be able to accept JSON.
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
 app.get("/api/data", getForm);
-app.get("/api/pictures", getImg)
+app.get("/api/pictures", getImg);
+app.get("/api/items", getItem);
+app.post("/api/items", addItem);
+app.delete("/api/items/:indexPos", deleteItem)
 
 app.listen(4000, () => console.log("Server running on 4000"));
